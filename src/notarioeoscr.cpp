895e71f585ff4650956aeee90eb7050674f99d63
Ruben <rubenabix@gmail.com>
2020-04-27 22:06:33 -0600

    add title

--- src/notarioeoscr.cpp
+++ src/notarioeoscr.cpp
@@ -2,7 +2,8 @@
 #include <eosio/transaction.hpp>
 #include <eosio/crypto.hpp>
 
-ACTION notarioeoscr::anotar(name usuario, const checksum256 &hash, bool guardar_en_tabla, string comentario, string contenido) {
+ACTION notarioeoscr::anotar(name usuario, const checksum256 &hash, bool guardar_en_tabla, string titulo, string contenido, string comentario)
+{
   require_auth(usuario);
   if (!guardar_en_tabla)
   {
@@ -20,21 +21,23 @@ ACTION notarioeoscr::anotar(name usuario, const checksum256 &hash, bool guardar_
   char *tx = (char *)malloc(s);
   read_transaction(tx, s);
   auto txid = sha256(tx, s);
-  _records.emplace(usuario, [&](auto& row) {
+  _records.emplace(usuario, [&](auto &row) {
     row.id = _records.available_primary_key();
     row.hash = hash;
     row.tx = txid;
   });
 }
 
-ACTION notarioeoscr::limpiar() {
+ACTION notarioeoscr::limpiar()
+{
   // DEV only
   require_auth(get_self());
 
   book_table _records(get_self(), get_self().value);
 
   auto records_itr = _records.begin();
-  while (records_itr != _records.end()) {
+  while (records_itr != _records.end())
+  {
     records_itr = _records.erase(records_itr);
   }
 }
