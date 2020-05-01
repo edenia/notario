9a0b220cb92c2197aaad53de356a6a8e82722b8f
Ruben <rubenabix@gmail.com>
2020-04-30 21:54:40 -0600

    add example ricardian generation

---
+++ ricardian/include/notarioeoscr.hpp
@@ -0,0 +1,28 @@
+#include <eosio/eosio.hpp>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT notarioeoscr : public contract
+{
+public:
+  using contract::contract;
+
+  ACTION anotar(name usuario, const checksum256 &hash, bool guardar_en_tabla, string titulo, string contenido, string comentario);
+  ACTION limpiar();
+
+private:
+  TABLE book
+  {
+    uint64_t id;
+    checksum256 hash;
+    checksum256 tx;
+    auto primary_key() const { return id; }
+    checksum256 get_hash() const { return hash; }
+    EOSLIB_SERIALIZE(book,
+                     (id)(hash)(tx));
+  };
+  typedef multi_index<name("libro"), book,
+                      indexed_by<"porhash"_n, const_mem_fun<book, checksum256, &book::get_hash>>>
+      book_table;
+};
---
+++ ricardian/ricardian/notarioeoscr.contracts.md
@@ -0,0 +1,7 @@
+<h1 class="contract"> anotar </h1>
+
+Anotar C
+
+<h1 class="clause">ArbitratorsFeesToPrevailingParty</h1>
+
+Limpiar C
