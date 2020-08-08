10bcec6366dfe37de7909c1078252df28379b22e
JustinCast <justincastillovalladares@gmail.com>
2020-08-08 16:33:10 -0600

    wip: work on issuance verifier implementation

--- src/routes/Dashboard/VerifyModal.js
+++ src/routes/Dashboard/VerifyModal.js
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types'
 import QrReader from 'react-qr-scanner'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
-import { InputHash } from '@eoscostarica/eoscr-components'
+import { InputHash, IssuanceVerifier } from '@eoscostarica/eoscr-components'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
 import Modal from '../../components/Modal'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
 
 const VerifyModal = ({
   classes,
   openVerifyModal,
   setOpenVerifyModal,
+  setVerificationMethod,
+  verificationMethod,
+  methods,
   setInputHashValue,
   inputHashValue,
   setLoadingQr,
@@ -23,101 +28,146 @@ const VerifyModal = ({
   setError,
   error,
   onHandleVerify
-}) => (
-  <Modal openModal={openVerifyModal} setOpenModal={setOpenVerifyModal}>
-    <Box className={classes.verifyContent}>
-      <Box>
-        <Typography variant="h3" align="left">
-          {t('notary.verifyModal.title')}
-        </Typography>
-        <Typography variant="body1">
-          {t('notary.verifyModal.paragraph')}
-        </Typography>
-        <InputHash
-          useHashValidator
-          id="input-hash"
-          label="Hash"
-          variant="outlined"
-          placeholder="Hash del certificado"
-          value={inputHashValue.hash}
-          handleOnChange={(result) => setInputHashValue(result)}
-          fullWidth
-          messageError="Incorrect hash"
-          messageSuccess="Hash is correct"
-        />
-        {!loadingQr && (
-          <Box className={classes.btnShowQr}>
+}) => {
+  const getComponent = (verificationMethod) => {
+    switch (verificationMethod) {
+      case 'rawHash': {
+        return (
+          <Box>
+            <Typography variant="h3" align="left">
+              {t('notary.verifyModal.title')}
+            </Typography>
+            <Typography variant="body1">
+              {t('notary.verifyModal.paragraph')}
+            </Typography>
+            <InputHash
+              useHashValidator
+              id="input-hash"
+              label="Hash"
+              variant="outlined"
+              placeholder="Hash del certificado"
+              value={inputHashValue.hash}
+              handleOnChange={(result) => setInputHashValue(result)}
+              fullWidth
+              messageError="Incorrect hash"
+              messageSuccess="Hash is correct"
+            />
+            {!loadingQr && (
+              <Box className={classes.btnShowQr}>
+                <Button
+                  variant="outlined"
+                  startIcon={<CameraAltIcon />}
+                  onClick={() => setLoadingQr(true)}
+                >
+                  {t('notary.verifyModal.qrButton')}
+                </Button>
+              </Box>
+            )}
+            {loadingQr && (
+              <Box className={classes.qrBox}>
+                <Box className={classes.closeIcon}>
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => setLoadingQr(false)}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                </Box>
+                <QrReader
+                  delay={100}
+                  onError={() => {}}
+                  facingMode="rear"
+                  style={{
+                    height: 300,
+                    width: '100%',
+                    backgroundColor: '#8080808c',
+                    marginBottom: 24
+                  }}
+                  onScan={(value) => {
+                    if (!value) return
+
+                    setInputHashValue(value)
+                    setLoadingQr(false)
+                  }}
+                />
+              </Box>
+            )}
+          </Box>
+        )
+      }
+      case 'uploadFile': {
+        return <IssuanceVerifier />
+      }
+      default:
+        break
+    }
+  }
+
+  return (
+    <Modal openModal={openVerifyModal} setOpenModal={setOpenVerifyModal}>
+      <Box className={classes.verifyContent}>
+        <Box>
+          <TextField
+            select
+            label={t('notary.verifyIssuance.selectLabel')}
+            helperText={
+              verificationMethod
+                ? ''
+                : t('notary.verifyIssuance.selectVerificationMethod')
+            }
+            value={verificationMethod}
+            onChange={(e) => setVerificationMethod(e.target.value)}
+            variant="outlined"
+            fullWidth
+            disabled={error.isError}
+          >
+            {methods.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          {getComponent(verificationMethod)}
+
+          <Box className={classes.contentBtn}>
             <Button
               variant="outlined"
-              startIcon={<CameraAltIcon />}
-              onClick={() => setLoadingQr(true)}
+              onClick={() => setOpenVerifyModal(false)}
             >
-              {t('notary.verifyModal.qrButton')}
+              {t('notary.cancelButton')}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => onHandleVerify()}
+              disabled={!inputHashValue.isValid}
+            >
+              {t('notary.acceptButton')}
             </Button>
           </Box>
-        )}
-        {loadingQr && (
-          <Box className={classes.qrBox}>
-            <Box className={classes.closeIcon}>
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => setLoadingQr(false)}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            </Box>
-            <QrReader
-              delay={100}
-              onError={() => {}}
-              facingMode="rear"
-              style={{
-                height: 300,
-                width: '100%',
-                backgroundColor: '#8080808c',
-                marginBottom: 24
-              }}
-              onScan={(value) => {
-                if (!value) return
-
-                setInputHashValue(value)
-                setLoadingQr(false)
-              }}
-            />
-          </Box>
-        )}
-      </Box>
-      <Box>
-        <Box className={classes.contentBtn}>
-          <Button variant="outlined" onClick={() => setOpenVerifyModal(false)}>
-            {t('notary.cancelButton')}
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => onHandleVerify()}
-            disabled={!inputHashValue.isValid}
-          >
-            {t('notary.acceptButton')}
-          </Button>
         </Box>
       </Box>
-    </Box>
-  </Modal>
-)
+    </Modal>
+  )
+}
 
 VerifyModal.propTypes = {
   classes: PropTypes.object,
   openVerifyModal: PropTypes.bool,
   setOpenVerifyModal: PropTypes.func,
+  setVerificationMethod: PropTypes.func,
+  verificationMethod: PropTypes.string,
+  methods: PropTypes.array,
   setInputHashValue: PropTypes.func,
   inputHashValue: PropTypes.object,
   setLoadingQr: PropTypes.func,
   loadingQr: PropTypes.bool,
   t: PropTypes.any,
-  error: PropTypes.object,
   setError: PropTypes.func,
+  error: PropTypes.object,
   onHandleVerify: PropTypes.func
 }
 
